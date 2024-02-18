0c2b66c2371d79cc0fef18507e5abfd3e34f6eda
chloe k <chloe.ksh.kim@gmail.com>
2024-02-17 21:41:00 -0500

    Add persistent locale storage using AsyncStorage in i18n setup

--- src/components/RandomPhrase.js
+++ src/components/RandomPhrase.js
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 const getRandomCookieKey = () => {
   const len = 15;
   const randomNum = Math.floor(Math.random() * len);
-  console.log("Math.random()", Math.random());
-  console.log("randomNum", randomNum);
+  // console.log("Math.random()", Math.random());
+  // console.log("randomNum", randomNum);
   return `cookie_${randomNum + 1}`;
 };
 
--- src/use-translation.js
+++ src/use-translation.js
@@ -1,6 +1,7 @@
 import { getLocales } from "expo-localization";
 import { I18n } from "i18n-js";
 import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ko = require("./lang/lang.ko.json");
 const en = require("./lang/lang.en.json");
@@ -18,11 +19,28 @@ const i18n = new I18n({
 
 const deviceLanguage = getLocales()[0].languageCode;
 
+const LOCALE_KEY = "locale2";
+
 export const useTranslation = () => {
-  const [locale, setLocale] = useState(null);
+  const [locale, _setLocale] = useState(null);
+
+  const setLocale = (v) => {
+    _setLocale(v);
+    AsyncStorage.setItem(LOCALE_KEY, v);
+  };
+
+  const init = async () => {
+    const fs = await AsyncStorage.getItem(LOCALE_KEY);
+    if (fs !== null) {
+      _setLocale(fs);
+    } else {
+      setLocale(deviceLanguage);
+    }
+    // console.log("fs", fs);
+  };
 
   useEffect(() => {
-    setLocale(deviceLanguage);
+    init();
   }, []);
 
   return {
